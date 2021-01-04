f519d3e8cb8f155751bc058e440f410998c3e213
Emma-prn <60692797+Emma-prn@users.noreply.github.com>
2021-01-04 15:38:41 +0100

    Add files via upload
    Création Plateau et ajout pions positions de départ

--- Board.cpp
+++ Board.cpp
@@ -4,13 +4,19 @@ using namespace std;
 void displayBoard(char Board[8][8]);
 
 int main() {
-    char Board[8][8];
+    char Board[8][8], A, B;
     int i(0),j(0);
     for(i=0;i<8;i++) 
     { 
         for(j=0;j<8;j++) 
         Board[i][j]=' '; 
     } 
+    cout<<"Enter Your Token Symbol: "; cin>>A; 
+    cout<<"Enter Opponent's Token Symbol: "; cin>>B;
+    Board[3][3] = A; 
+    Board[3][4] = B; 
+    Board[4][3] = B; 
+    Board[4][4] = A;
     displayBoard(Board);
 
 }
