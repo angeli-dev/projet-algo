2e4c10e8a9f7cf4a5fd3f7654af32f6c5d883521
angeli-dev <angeliquelebas2008@gmail.com>
2021-01-17 14:49:09 +0100

    update appelFonctions.h
    captureJetons

--- appelFonctions.h
+++ appelFonctions.h
@@ -1,3 +1,4 @@
 void displayBoard(char Board[8][8]);
 void initJoueur(Jeu *jeu, Joueur *j1, Joueur *j2);
 void nouveauJeton(char Board[8][8], Joueur joueurActuel);
+void captureJetons(char Board[8][8], Joueur joueurActuel, Jeton nouveauJeton);
