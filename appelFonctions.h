a68622de422005088360e19fb024e7f6e5d01b07
angeli-dev <angeliquelebas2008@gmail.com>
2021-01-18 17:09:00 +0100

    a tester

--- appelFonctions.h
+++ appelFonctions.h
@@ -1,3 +1,8 @@
 void displayBoard(char Board[8][8]);
 void initJoueur(Jeu *jeu, Joueur *j1, Joueur *j2);
-void nouveauJeton(char Board[8][8]);
+Jeton *initJeton(Jeton *Board[8][8], Joueur joueurActif, int posY, int posX);
+Jeton *nouveauJeton(Jeton *Board[8][8], Joueur joueurActif);
+void captureJetons(Jeton *Board[8][8], Joueur joueurActif, Joueur Passif, Jeton nouveauJeton);
+void captureDroite(Jeton *Board[8][8], Joueur joueurActif, Joueur Passif, Jeton nouveauJeton);
+void captureGauche(Jeton *Board[8][8], Joueur joueurActif, Joueur Passif, Jeton nouveauJeton);
+void captureHaut(Jeton *Board[8][8], Joueur joueurActif, Joueur Passif, Jeton nouveauJeton);
